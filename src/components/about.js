32ce02c88d0d73c1d75460cdd79046bfcef8c4f0
Tommy Coleman <tommycoleman@Tommys-MacBook-Pro.local>
2020-04-30 19:23:13 -0400

    formated header and about for mobile view

--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import profile_picture from '../assetts/profile_picture.jpg'
+import '../css/about.css'
 
 function About() {
     return (
-        <section>
-            <img src={profile_picture} alt="professional picture of Tommy Coleman"/>
-            <h1>
-                Tommy Coleman
-            </h1>
-            <h2>
-                Full Stack Web Developer
-            </h2>
-            <p>
-                Hi, I'm Tommy Coleman. 
-                I'm a full stack web developer. 
-                Look below to see some of my work.
-            </p>
+        <section className='about'>
+            <img src={profile_picture} alt="professional picture of Tommy Coleman" className='profile-picture'/>
+            <div className="about-container">
+                <h1>
+                    Tommy Coleman
+                </h1>
+                <h2>
+                    Full Stack Web Developer
+                </h2>
+                <p>
+                    Hi, I'm Tommy Coleman. <br />
+                    I'm a full stack web developer. <br />
+                    Look below to see some of my work.
+                </p>
+                <button>
+                    PROJECTS
+                </button>
+            </div>
         </section>
     )
 }
